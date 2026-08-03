6da5096b6fe7c524e58a647f4d7cda92ed511f57
f025954954-coder <f025954954@gmail.com>
2026-08-03 20:05:55 +0300

    Integrate Google Analytics tracking script
    Added Google Analytics tracking script to the application.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/* Google Analytics (gtag.js) */
+(function(){var id='G-MP6RWXQK18';var s=document.createElement('script');s.async=true;s.src='https://www.googletagmanager.com/gtag/js?id='+id;document.head.appendChild(s);window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}window.gtag=gtag;gtag('js',new Date());gtag('config',id);})();
 /* ===== לקסקור — JS משותף לכל העמודים ===== */
 (function(){
   // icons
